7f2fa2e826dfe94207f61d8e45a0bfcde44a4d0f
Yazan Aqel <yazan.aqel93@gmail.com>
2026-01-30 16:22:59 +0300

    new portfolio

---
+++ js/main.js
@@ -0,0 +1,123 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Typed Initiate
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+
+
+    // Smooth scrolling to section
+    $(".btn-scroll").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 0
+            }, 1500, 'easeInOutExpo');
+        }
+    });
+    
+    
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+})(jQuery);
+
+const form = document.querySelector('.mailForm');
+  
+function SendMessage(e){
+  e.preventDefault();
+
+  namee = document.querySelector('.txtName');
+  tel = document.querySelector('.txtTel');
+  sub = document.querySelector('.txtSubject');
+  email = document.querySelector('.txtMail');
+  msg = document.querySelector('.txtMessage');
+
+Email.send({
+SecureToken : "5564c1bf-e522-4572-9320-55559193007d",
+To : 'yazan.aqel93@gmail.com',
+From : 'yazanakel107@gmail.com',
+Subject : 'From portfolio',
+Body : namee.value + '<br><hr/>'+sub.value+ '<br><hr/>' + email.value + '<br><hr/>' + msg.value + '<br><hr/>' + tel.value
+}).then(
+message => alert('thank you for emailing me :)'));
+  
+  namee.value='';
+      tel.value='';
+      sub.value = '';
+      email.value = '';
+      msg.value = '';
+}
+
+form.addEventListener('submit',SendMessage);
